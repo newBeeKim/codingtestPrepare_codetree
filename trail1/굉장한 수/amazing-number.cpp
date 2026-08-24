b2c8f322060addb17a50d23780a08430e77b4ac6
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-24 10:36:38 +0900

    [Passed] 굉장한 수 (2ms, 0MB)

---
+++ trail1/굉장한 수/amazing-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if((N % 3 == 0 && N % 2 != 0) || (N % 2 == 0 && N % 5 == 0)){
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+    return 0;
+}
