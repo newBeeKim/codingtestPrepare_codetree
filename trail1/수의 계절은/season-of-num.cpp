6ce852547e210e3557a83eade69ba53920aca650
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-25 10:11:40 +0900

    [Passed] 수의 계절은 (1ms, 0MB)

---
+++ trail1/수의 계절은/season-of-num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M;
+    cin >> M;
+
+    if(M >= 3 && M <= 5) {
+        cout << "Spring";
+    } else if(M >= 6 && M <= 8) {
+        cout << "Summer";
+    } else if(M >= 9 && M <= 11) {
+        cout << "Fall";
+    } else if((M >= 1 && M <= 2) || M == 12) {
+        cout << "Winter";
+    }
+    
+
+    return 0;
+}
