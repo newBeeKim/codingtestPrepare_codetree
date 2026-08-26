b577745cdc099cfdc1d452f7789224e7e264ba1b
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-26 13:31:41 +0900

    [Passed] 일 수 구하기 (2ms, 0MB)

---
+++ trail1/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n == 2){
+        cout << 28;
+    } else if((n == 1) || (n == 3) || (n == 5) || (n == 7) || (n == 8) || (n == 10) || (n == 12)){
+        cout << 31;
+    } else if((n == 2) || (n == 4) || (n == 6) || (n == 9) || (n == 11)){
+        cout << 30;
+    }
+    return 0;
+}
