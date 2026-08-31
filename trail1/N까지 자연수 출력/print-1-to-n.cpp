a6f55815470fb48487ba228f82caa9ff5e3762b4
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-31 10:30:20 +0900

    [Passed] N까지 자연수 출력 (2ms, 0MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int i = 1;
+    while(i <= N){
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
