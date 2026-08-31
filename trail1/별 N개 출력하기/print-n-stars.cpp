5b1c4f8c5fe9d9309126caa8dae95e55deb58ed1
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-31 10:31:32 +0900

    [Passed] 별 N개 출력하기 (2ms, 0MB)

---
+++ trail1/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int i = 1;
+
+    while(i <= N){
+        cout << "*" << endl;
+        i++;
+    }
+    return 0;
+}
