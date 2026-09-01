3d2fdf9e623a52b34295c1604c6ac239594af586
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-09-01 11:01:38 +0900

    [Passed] 짝수만 출력 (2ms, 0MB)

---
+++ trail1/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    cin >> A >> B;
+    int i = A;
+
+    while(i <= B){
+        if(i % 2 == 0){
+            cout << i << " ";
+        }
+        i++;
+    }
+    return 0;
+}
