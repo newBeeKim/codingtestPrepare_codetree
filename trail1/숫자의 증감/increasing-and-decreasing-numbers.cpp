646c944eba9cfeab4f9a19866c5fbbf6c9d916b3
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-09-04 10:08:43 +0900

    [Passed] 숫자의 증감 (1ms, 0MB)

---
+++ trail1/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char C;
+    int N;
+    cin >> C >> N;
+
+    if(C == 'A'){
+        for(int i = 1; i <= N; i++){
+            cout << i << " ";
+        }
+    } else if(C == 'D'){
+        for(int i = N; i >= 1; i--){
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
