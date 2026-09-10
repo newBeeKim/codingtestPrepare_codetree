7c160fb15dacb336df96519921c9b6577100df0b
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-09-10 14:15:17 +0900

    [Passed] a의 배수 (1ms, 0MB)

---
+++ trail1/a의 배수/multiple-of-a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, a;
+    cin >> N >> a;
+    int i = 1;
+
+    while(i <= N){
+        if(i % a == 0){
+            cout << 1 << endl;
+        } else {
+            cout << 0 << endl;
+        }
+        i++;
+    }
+    return 0;
+}
