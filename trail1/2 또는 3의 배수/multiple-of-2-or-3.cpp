a672bc1455cb04e8a454185f3c8d652196ad7be8
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-09-10 14:11:55 +0900

    [Passed] 2 또는 3의 배수 (2ms, 0MB)

---
+++ trail1/2 또는 3의 배수/multiple-of-2-or-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    for(int i = 1; i <= N; i++){
+        if(i % 2 == 0 || i % 3 == 0){
+            cout << 1 << " ";
+        } else {
+            cout << 0 << " ";
+        }
+    }
+    return 0;
+}
