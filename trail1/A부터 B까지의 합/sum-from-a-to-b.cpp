c66b5da9df21eacb114e3d5634ac643b98a18a04
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-09-14 10:12:03 +0900

    [Passed] A부터 B까지의 합 (2ms, 0MB)

---
+++ trail1/A부터 B까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    cin >> A >> B;
+    
+    int sum = 0;
+    for(int i = A; i <= B; i++){
+        sum += i;
+    }
+    cout << sum;
+    return 0;
+}
