32bf6b4eae0ad023d608cb9900aab34c9a296ecb
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-09-15 10:33:53 +0900

    [Passed] 총합 구하기 (2ms, 0MB)

---
+++ trail1/총합 구하기/find-the-total.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, sum = 0;
+    cin >> A >> B;
+
+    for(int i = A; i <= B; i++){
+        if(i % 6 == 0 && i % 8 != 0){
+            sum += i;
+        }
+    }
+    cout << sum;
+    return 0;
+}
