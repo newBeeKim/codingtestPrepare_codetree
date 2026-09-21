0b6f5790f7862f4e90044d4589851c8391660cca
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-09-21 13:47:22 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ trail1/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 1;
+    while(num != 0){
+        cin >> num;
+        if(num == 0) {
+            break;
+        }
+        cout << num << endl;
+    }
+    return 0;
+}
